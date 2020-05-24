8a9c7da338136f15af776439494e3b4ec4dd5826
Austin Oie <austinoie@gmail.com>
2020-05-24 17:20:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var lowercase = 'hello!'
 var mixedCase = 'Hi there!'
 
 function sayHiToGrandma(string) {
-  if (lowercase.toLowerCase() === lowercase)
+  if (lowercase.toLowerCase()) === lowercase 
     return "I can't hear you!"
   else if (uppercase.toUpperCase() === uppercase)
     return "YES INDEED!"
