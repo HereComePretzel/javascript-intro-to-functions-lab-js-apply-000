9883764e1da798837604c5d3d62f36b8822e8041
Austin Oie <austinoie@gmail.com>
2020-05-24 16:55:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function logWhisper(string) {
   console.log('hello');
 }
 
-
+var uppercase = 'HELLO!'
+var lowercase = "hello!"
 function sayHiToGrandma(string) {
   if lowercase.toLowerCase() === lowercase
   return "I can't hear you!";
