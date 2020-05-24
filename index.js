43f0cd78a70091377ffa5e2a28aef92e5f6a5bb5
Austin Oie <austinoie@gmail.com>
2020-05-24 17:26:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string) {
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   } else {
-    return "I love you too."
+    return "I love you, too."
   }
 }
